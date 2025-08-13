e0c73f37e80c2890a7db5ad24b02b8370fca7299
amit <amitbh94@gmail.com>
2025-08-13 15:30:39 +0530

    Xml configuration(first Configuration MetaData) based SpringBoot app, Initial commit.

---
+++ SpringDemo/src/main/java/com/codedecode/SpringDemo/Demos/Car.java
@@ -0,0 +1,11 @@
+package com.codedecode.SpringDemo.Demos;
+
+public class Car {
+	
+	Engine engine;
+	
+	public String getEngineData() {
+		return engine.name;
+	}
+
+}
---
+++ SpringDemo/src/main/java/com/codedecode/SpringDemo/Demos/Engine.java
@@ -0,0 +1,7 @@
+package com.codedecode.SpringDemo.Demos;
+
+public class Engine {
+	
+	public String name="Hi I am 1.5 liter vtech Engine";
+
+}
