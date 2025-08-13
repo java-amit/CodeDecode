1ab35be195a953c5ff4839fb6defe2814f48b332
amit <amitbh94@gmail.com>
2025-08-13 17:22:01 +0530

    setter injection

--- SpringDemo/src/main/java/com/codedecode/SpringDemo/Demos/Car.java
+++ SpringDemo/src/main/java/com/codedecode/SpringDemo/Demos/Car.java
@@ -3,7 +3,21 @@ package com.codedecode.SpringDemo.Demos;
 public class Car {
 	
 	Engine engine;
-	
+
+
+
+	public Engine getEngine() {
+		return engine;
+	}
+
+
+
+	public void setEngine(Engine engine) {
+		this.engine = engine;
+	}
+
+
+
 	public String getEngineData() {
 		return engine.name;
 	}
--- SpringDemo/src/main/java/com/codedecode/SpringDemo/SpringDemoApplication.java
+++ SpringDemo/src/main/java/com/codedecode/SpringDemo/SpringDemoApplication.java
@@ -2,7 +2,9 @@ package com.codedecode.SpringDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.codedecode.SpringDemo.Demos.Car;
 
@@ -10,7 +12,10 @@ import com.codedecode.SpringDemo.Demos.Car;
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context =  SpringApplication.run(SpringDemoApplication.class, args);
+//		ConfigurableApplicationContext context =  SpringApplication.run(SpringDemoApplication.class, args);
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationcontext.xml");
+		
 		Car car = context.getBean(Car.class);
 		System.out.println(car.getEngineData());
 	}
