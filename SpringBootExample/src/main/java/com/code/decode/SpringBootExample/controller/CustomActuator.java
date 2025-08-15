a2a6d9adac9849d2c0764fc8625695eba2c03076
amit <amitbh94@gmail.com>
2025-08-15 18:30:54 +0530

    creating custom actuator for db or other information

---
+++ SpringBootExample/src/main/java/com/code/decode/SpringBootExample/controller/CustomActuator.java
@@ -0,0 +1,17 @@
+package com.code.decode.SpringBootExample.controller;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+
+@Component
+@Endpoint(id = "customActuator")
+public class CustomActuator {
+	
+	@ReadOperation
+	public String currentDbDetails() {
+		return "Give current DB status of the application";
+	}
+	
+
+}
