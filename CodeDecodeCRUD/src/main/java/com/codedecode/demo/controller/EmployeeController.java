57b2f4c948cef8337ba0edb390252f57bfbfdfaf
amit <amitbh94@gmail.com>
2025-08-17 14:07:21 +0530

    Adding object in DB: 1. Add EmployeeServiceInterface, implements EmployeeService 2. Add Post method : addEmployee() in controller 3. add addEmployee in service method and call from repo interface : crudRepo.save(employee);

--- CodeDecodeCRUD/src/main/java/com/codedecode/demo/controller/EmployeeController.java
+++ CodeDecodeCRUD/src/main/java/com/codedecode/demo/controller/EmployeeController.java
@@ -3,24 +3,31 @@ package com.codedecode.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.codedecode.demo.entity.Employee;
-import com.codedecode.demo.repo.EmployeeCrudRepo;
+import com.codedecode.demo.service.EmployeeServiceInterface;
 
 @RestController
 @RequestMapping("/code")
 public class EmployeeController {
 	
 	@Autowired
-	EmployeeCrudRepo empCrud;
+	private EmployeeServiceInterface employeeServiceInterface;  ;
 	
-	@GetMapping
-	public List<Employee> getAllEmployees(){
-		System.out.println("Getting All Employee");
-		return empCrud.findAll();
+	
+	@PostMapping("/save")
+	public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee){
+		Employee employeesaved = employeeServiceInterface.addEmployee(employee);
+		return new ResponseEntity<Employee>(employeesaved,HttpStatus.CREATED);
 	}
+	
+	
 
 }
--- CodeDecodeCRUD/src/main/java/com/codedecode/demo/service/EmployeeService.java
+++ CodeDecodeCRUD/src/main/java/com/codedecode/demo/service/EmployeeService.java
@@ -1,8 +1,23 @@
 package com.codedecode.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class EmployeeService {
+import com.codedecode.demo.entity.Employee;
+import com.codedecode.demo.repo.EmployeeCrudRepo;
 
+@Service
+public class EmployeeService implements EmployeeServiceInterface{
+
+	@Autowired
+	private EmployeeCrudRepo crudRepo;
+
+	@Override
+	public Employee addEmployee(Employee employee) {
+		Employee saveEmployee = crudRepo.save(employee);
+		return saveEmployee;
+	}
+	
+	
+	
 }
