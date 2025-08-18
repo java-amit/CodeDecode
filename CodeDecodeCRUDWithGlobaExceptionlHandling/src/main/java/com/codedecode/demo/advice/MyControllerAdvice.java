d5ddf94ee48877f66fb904b11dd303d02fe8f17f
amit <amitbh94@gmail.com>
2025-08-18 19:12:20 +0530

    1. MyControllerAdvice extends ResponseEntityExceptionHandler 2. override handleHttpRequestMethodNotSupported method to handle wrong request , e.g. method is post but you send get request

--- CodeDecodeCRUDWithGlobaExceptionlHandling/src/main/java/com/codedecode/demo/advice/MyControllerAdvice.java
+++ CodeDecodeCRUDWithGlobaExceptionlHandling/src/main/java/com/codedecode/demo/advice/MyControllerAdvice.java
@@ -2,10 +2,14 @@ package com.codedecode.demo.advice;
 
 import java.util.NoSuchElementException;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.codedecode.demo.custom.exception.EmptyInputException;
@@ -24,6 +28,13 @@ public class MyControllerAdvice extends ResponseEntityExceptionHandler{
 	public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException elementException) {
 
 		return new ResponseEntity<String>("No Value is present in DB, Please change you request", HttpStatus.NOT_FOUND);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(HttpRequestMethodNotSupportedException ex,
+			HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		// TODO Auto-generated method stub
+		return new ResponseEntity<Object>("Please change http method type", HttpStatus.NOT_FOUND);
 	}
 
 }
