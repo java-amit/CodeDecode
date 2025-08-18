d27b3d28ea3aac5a92494ba20e8afc2b58c63875
amit <amitbh94@gmail.com>
2025-08-18 18:40:09 +0530

    MyControllerAdvice class ko extend jarur kre MyControllerAdvice extends ResponseEntityExceptionHandler

--- CodeDecodeCRUDWithGlobaExceptionlHandling/src/main/java/com/codedecode/demo/advice/MyControllerAdvice.java
+++ CodeDecodeCRUDWithGlobaExceptionlHandling/src/main/java/com/codedecode/demo/advice/MyControllerAdvice.java
@@ -6,11 +6,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.codedecode.demo.custom.exception.EmptyInputException;
 
 @ControllerAdvice
-public class MyControllerAdvice {
+public class MyControllerAdvice extends ResponseEntityExceptionHandler{
 
 	@ExceptionHandler(EmptyInputException.class) // Giving a class as an argument inside an annotation is called a Class
 													// literal (or class reference
