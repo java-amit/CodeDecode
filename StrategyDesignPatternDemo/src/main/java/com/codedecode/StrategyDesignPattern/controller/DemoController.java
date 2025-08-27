a025e6da72016ee9cfcb92b8b46a06f2734313a1
amit <amitbh94@gmail.com>
2025-08-27 20:00:01 +0530

    Now, we have to test our code so need to create Controller.

---
+++ StrategyDesignPatternDemo/src/main/java/com/codedecode/StrategyDesignPattern/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.codedecode.StrategyDesignPattern.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codedecode.StrategyDesignPattern.enums.EncodingPatternEnum;
+import com.codedecode.StrategyDesignPattern.factory.EncryptionFactory;
+
+@RestController
+public class DemoController {
+	
+	@Autowired
+	EncryptionFactory factory;
+	
+	public void encryptWithEncoder(@RequestParam EncodingPatternEnum encodingPatternEnum) {
+		factory.findEncryptionType(encodingPatternEnum);
+	}
+
+}
