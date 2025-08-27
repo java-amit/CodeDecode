34f3834b5deb6d1e2c97d1d518d11707c324fe46
amit <amitbh94@gmail.com>
2025-08-27 21:29:25 +0530

    call encrypt("code decode") method with Encryption

--- StrategyDesignPatternDemo/src/main/java/com/codedecode/StrategyDesignPattern/controller/DemoController.java
+++ StrategyDesignPatternDemo/src/main/java/com/codedecode/StrategyDesignPattern/controller/DemoController.java
@@ -1,6 +1,7 @@
 package com.codedecode.StrategyDesignPattern.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +14,9 @@ public class DemoController {
 	@Autowired
 	EncryptionFactory factory;
 	
+	@GetMapping("/encrypt")
 	public void encryptWithEncoder(@RequestParam EncodingPatternEnum encodingPatternEnum) {
-		factory.findEncryptionType(encodingPatternEnum);
+		factory.findEncryptionType(encodingPatternEnum).encrypt("code decode");
 	}
 
 }
