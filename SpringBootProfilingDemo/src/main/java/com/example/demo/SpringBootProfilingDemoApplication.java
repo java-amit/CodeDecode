d86563a289d26a1fda67a4751919650a84f7615a
amit <amitbh94@gmail.com>
2025-08-31 14:38:13 +0530

    Initial Commit : profile in spring boot

---
+++ SpringBootProfilingDemo/src/main/java/com/example/demo/SpringBootProfilingDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProfilingDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProfilingDemoApplication.class, args);
+	}
+
+}
