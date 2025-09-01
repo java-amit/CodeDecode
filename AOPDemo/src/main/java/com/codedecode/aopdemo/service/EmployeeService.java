c1e16f579258a9ff4210535c3508222ad61f5953
amit <amitbh94@gmail.com>
2025-09-01 17:02:18 +0530

    Service is ready

--- AOPDemo/src/main/java/com/codedecode/aopdemo/service/EmployeeService.java
+++ AOPDemo/src/main/java/com/codedecode/aopdemo/service/EmployeeService.java
@@ -1,8 +1,11 @@
 package com.codedecode.aopdemo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.codedecode.aopdemo.entity.Employee;
 import com.codedecode.aopdemo.repo.EmployeeRepo;
 
 @Service
@@ -10,5 +13,13 @@ public class EmployeeService {
 	
 	@Autowired
 	EmployeeRepo employeeRepo;
+
+	public List<Employee> getAllEmps() {
+		return employeeRepo.findAll();
+	}
+
+	public Employee addEmployee(Employee e) {
+		return employeeRepo.save(e);
+	}
 
 }
