a6e7e81abefd5cf173b185a5ee404af4f684d3e9
amit <amitbh94@gmail.com>
2025-09-01 16:08:56 +0530

    Controller is ready: for Create and Read Operation.

---
+++ AOPDemo/src/main/java/com/codedecode/aopdemo/controller/EmployeeController.java
@@ -0,0 +1,31 @@
+package com.codedecode.aopdemo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codedecode.aopdemo.entity.Employee;
+import com.codedecode.aopdemo.service.EmployeeService;
+
+@RestController
+@RequestMapping("/code")
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeService employeeService;
+	
+	@GetMapping("/all")
+	public ResponseEntity<?> fetchAllEmployees(){
+		return new ResponseEntity<List<Employee>>(employeeService.getAllEmps(), HttpStatus.OK);
+	}
+	
+	public ResponseEntity<?> addEmployee(@RequestBody Employee e){
+		return new ResponseEntity<Employee>(employeeService.addEmployee(e), HttpStatus.OK);
+	}
+}
---
+++ AOPDemo/src/main/java/com/codedecode/aopdemo/repo/EmployeeRepo.java
@@ -0,0 +1,11 @@
+package com.codedecode.aopdemo.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.codedecode.aopdemo.entity.Employee;
+
+@Repository
+public interface EmployeeRepo extends JpaRepository<Employee, Long>{
+
+}
