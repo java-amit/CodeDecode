85599a2f55a9cbb8c3b52d4945b1cf671ac55206
amit <amitbh94@gmail.com>
2025-09-06 18:06:30 +0530

    Initial commit

---
+++ SBValidationMapperDemo/src/main/java/com/codedecode/SBValidationMapperDemo/SbValidationMapperDemoApplication.java
@@ -0,0 +1,13 @@
+package com.codedecode.SBValidationMapperDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbValidationMapperDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbValidationMapperDemoApplication.class, args);
+	}
+
+}
---
+++ SBValidationMapperDemo/src/test/java/com/codedecode/SBValidationMapperDemo/SbValidationMapperDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codedecode.SBValidationMapperDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbValidationMapperDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
