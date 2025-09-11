6bba33eb0c4c72698ae117220b45f3af3b3a16cd
amit <amitbh94@gmail.com>
2025-09-11 15:12:56 +0530

    add spring-boot-starter-validation dependency in pom.xml then validation annotation will automatically complete

--- SBValidationMapperDemo/src/main/java/com/codedecode/SBValidationMapperDemo/DTO/EmployeeDTO.java
+++ SBValidationMapperDemo/src/main/java/com/codedecode/SBValidationMapperDemo/DTO/EmployeeDTO.java
@@ -4,6 +4,10 @@ import java.util.Date;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,10 +19,15 @@ public class EmployeeDTO {
 
 	private int id;
 	
+	@NotBlank(message = "Name cannot be blank")
 	private String name;
 	
+	@Min(value = 20, message = "MInimum age should be 20")
+	@Max(value = 100, message = "Max age should be 100")
 	private int age;
 	
+	@Email
+	@NotBlank(message = "Email should not be blank")
 	private String emailAddress;
 	
 	private int projectId;
